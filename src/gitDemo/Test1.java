3693d97f5368136a7a8c29ec93daed4f9bc5a296
DELL <DELL@DESKTOP-CRP4OM8>
2022-09-22 17:42:25 +0530

    My 1st commit to git local repository....

---
+++ src/gitDemo/Test1.java
@@ -0,0 +1,15 @@
+package gitDemo;
+
+public class Test1 {
+
+	public static void main(String args[]) {
+		String hungry="no";
+		
+		if(hungry!="yes") {
+			System.out.println("eat some food....");
+		}
+		else {
+			System.out.println("Do work...");
+		}
+	}
+}
